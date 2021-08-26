d5a4b9fed484d7ce067d19ce41ed8281b08ef293
Washington Obungu <tobungu@gmail.com>
2021-08-26 09:17:21 -0700

    Wrote program to output string using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *main - Program entry point
+ */
+int main(void)
+{
+	/**
+	 *print_hello - prints string to output
+	 */
+	void print_hello(void)
+	{
+		printf("with proper grammer, but the outcome is a piece of art,\
+\n");
+	}
+	return (0);
+}
