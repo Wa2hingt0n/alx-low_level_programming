a628717e22f46e4d7d8369aafbbc0d3f5b0f7d64
Washington Obungu <tobungu@gmail.com>
2021-08-26 08:46:16 -0700

    Added a function description

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,17 @@
-/*Program using the puts()*/
+/** 
+ * puts - write a line to the output
+ *
+ * Description: Its used to write a line or string to the output filestream.
+ * It also appends a newline character to output.
+ * Return: Always zero (success)
+*/
 
 #include <stdio.h>
 
-int main()
+int main(void)
 {
 	char my_string[] = "Programming is like building a multilingual puzzle";
-	puts(my_string);
-	return(0);
+
+	puts (my_string);
+	return (0);
 }
