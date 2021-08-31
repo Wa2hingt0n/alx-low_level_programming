a64bd4a626bd599443f9c76024254f439da09183
Washington Obungu <tobungu@gmail.com>
2021-08-31 06:13:22 -0700

    Wrote a function to check if character is lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - Checks for lowercase character
+ *
+ *Return: 1 if character is lowercase, 0 if otherwise
+ */
+
+int _islower(int c)
+{
+	int letter;
+
+	if ((c >= 97) && (c <= 122))
+		letter = 1;
+	else
+		letter = 0;
+
+	return (letter);
+}
