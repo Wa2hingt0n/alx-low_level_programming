e17e464cbb838a492e863023fb5420cd26e896b1
Washington Obungu <tobungu@gmail.com>
2021-09-01 11:30:49 -0700

    Wrote program to print the first 98 fibonacci numbers

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the first 98 numbers of the fibonacci sequence
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int n;
+	unsigned long i, j, sum;
+
+	i = 1;
+	j = 2;
+	sum = i + j;
+
+	printf("1, 2");
+
+	for (n = 1; n <= 96; n++)
+	{
+		printf(", %lu", sum);
+
+		i = j;
+		sum += j;
+		j = sum - i;
+	}
+	printf("\n");
+
+	return (0);
+}
