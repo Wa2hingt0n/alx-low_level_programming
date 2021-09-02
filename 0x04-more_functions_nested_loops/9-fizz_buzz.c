8dd52db415724e38ef8de31da84ac69c22dc8842
Washington Obungu <tobungu@gmail.com>
2021-09-02 09:13:21 -0700

    Removed space after last number

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -6,7 +6,6 @@
  * Description: Prints numbers from 0-100, where if number is multiple of 3,
  * print Fizz, if its a multiple of five, print Buzz, and if its a multiple of
  * both 3 and 5, prints FizzBuzz
- *
  * Return: Always 0
  */
 
@@ -22,6 +21,8 @@ int main(void)
 			printf("Buzz ");
 		else if ((n % 3 == 0) && (n % 5 == 0))
 			printf("FizzBuzz ");
+		else if (n == 100)
+			printf("Buzz");
 		else
 			printf("%d ", n);
 	}
