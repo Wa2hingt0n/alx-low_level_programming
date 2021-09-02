0eb3b1b5f9a777f6a240079b9541c21ac6172c3a
Washington Obungu <tobungu@gmail.com>
2021-09-02 08:52:38 -0700

    Wrote program to display numbers from 1-100

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers from 0-100
+ *
+ * Description: Prints numbers from 0-100, where if number is multiple of 3,
+ * print Fizz, if its a multiple of five, print Buzz, and if its a multiple of
+ * both 3 and 5, prints FizzBuzz
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 1; n <= 100; n++)
+	{
+		if ((n % 3 == 0) && (n % 5 != 0))
+			printf("Fizz ");
+		else if ((n % 5 == 0) && (n % 3 != 0))
+			printf("Buzz ");
+		else if ((n % 3 == 0) && (n % 5 == 0))
+			printf("FizzBuzz ");
+		else
+			printf("%d ", n);
+	}
+	printf("\n");
+
+	return (0);
+}
