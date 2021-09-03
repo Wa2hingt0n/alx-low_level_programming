59d2aeca8507e075f172b3eabf0c0ab1e02a773b
Washington Obungu <tobungu@gmail.com>
2021-09-03 03:24:47 -0700

    Modified final conditional statement

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -5,7 +5,7 @@
  * @a: first integer
  * @b: second integer
  * @c: third integer
- * Return: largest
+ * Return: largest number
  */
 
 int largest_number(int a, int b, int c)
@@ -20,7 +20,7 @@ int largest_number(int a, int b, int c)
 	{
 		largest = b;
 	}
-	else if (c > a && c > b)
+	else
 	{
 		largest = c;
 	}
