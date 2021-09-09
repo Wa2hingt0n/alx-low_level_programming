708d2a4ebc32145736bfe2e9471fe659a7397fc4
Washington Obungu <tobungu@gmail.com>
2021-09-09 12:55:35 -0700

    Updated the function

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,20 +10,11 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int len1, len2;
+	while (*s1 && *s2 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
 
-	len1 = 0;
-	len2 = 0;
-
-	while (s1[len1] != '\0')
-		len1++;
-	while (s2[len2] != '\0')
-		len2++;
-
-	if (len1 < len2)
-		return (-15);
-	else if (len1 == len2)
-		return (0);
-	else
-		return (15);
+	return (*s1 - *s2);
 }
