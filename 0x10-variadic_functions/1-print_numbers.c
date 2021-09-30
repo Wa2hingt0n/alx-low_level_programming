19a4419aea32f863b543e9f25c317a71e77debc2
Washington Obungu <tobungu@gmail.com>
2021-09-30 11:50:09 -0700

    Created a function that prints integers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,32 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - prints numbers
+ * @separator: String to be printed between numbers
+ * @n: Number of integers passed to the function
+ *
+ * Return: Nothing
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+
+	va_start(ap, n);
+	if (n == 0 || separator == NULL)
+		return;
+	for (i = 0; i < n; i++)
+	{
+		if (i == 0)
+		{
+			printf("%d", va_arg(ap, int));
+		}
+		else
+		{
+			printf("%s %d", separator, va_arg(ap, int));
+		}
+	}
+	printf("\n");
+}
