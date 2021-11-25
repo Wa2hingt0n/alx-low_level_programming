0e4f80f4ede0bc9d5f216a19a44562d9701a2c33
Washington Obungu <tobungu@gmail.com>
2021-11-25 12:41:33 -0800

    Added a function that inserts a new node at a given position

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -9,7 +9,7 @@
  * Return: Address of the new node if successful
  *         NULL of it failed to add a new node
  */
-dlistint *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *tmp = *h, *new;
 
